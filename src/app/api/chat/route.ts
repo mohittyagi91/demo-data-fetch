b100db710642f3dda55d0f8af9330f7f15886532
mohit tyagi <v-mohityagi@microsoft.com>
2025-05-12 10:55:14 +0530

    added api for ollama

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest } from "next/server";
+import ollama from "ollama";
+
+// app/api/post-data/route.js
+export async function POST(req:NextRequest) {
+  try {
+    const getResponse = await fetch('http://localhost:3000/api/user/SrBachchan');  // Update the URL if deployed
+    
+    // Check if the GET request was successful
+    if (!getResponse.ok) {
+      return new Response('Error calling GET API', { status: getResponse.status });
+    }
+
+    const getData = await getResponse.json();
+    console.log("response",`summerize ${JSON.stringify(getData)}`)
+
+    const response = await ollama.chat({
+      model: "gemma3:1b",
+      messages: [{ role: "user", content: "summerize twitter profile data "+`${JSON.stringify(getData)}`} ],
+    });
+    return new Response(
+      JSON.stringify({
+        message: response.message.content,
+      }),
+      {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  } catch (error) {
+    console.error('Error in POST route:', error);
+    return new Response('Internal Server Error', { status: 500 });
+  }
+}
